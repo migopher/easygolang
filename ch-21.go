159ae0474dcda3b00bc73752baceeddd7073c9c6
ixuzl <863582605@qq.com>
2019-01-11 09:38:16 +0800

    更新switch 语句

--- ch-21.go
+++ ch-21.go
@@ -1,5 +1,28 @@
 package main
 
-func main()  {
-	
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	a := 30
+	switch a {
+	case 18:
+		fmt.Println("my age:" +strconv.Itoa(a))
+	case 24:
+		fmt.Println("my age:" +strconv.Itoa(a))
+	default:
+		fmt.Println("my age:" +strconv.Itoa(a))
+	}
+	switch  {
+	case a<18:
+		fmt.Println("少年")
+	case a>=18 && a<30:
+		fmt.Println("青年")
+	case a>=30 && a<40:
+		fmt.Println("中年")
+	default:
+		fmt.Println("老年")
+	}
 }
